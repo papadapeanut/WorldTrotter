812463f8b238c806ad388cb5b30e0d0fa88bbf46
papadapeanut <papadapeanut@gmail>
2017-06-03 22:00:32 -0400

    initial

---
+++ ConversionViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Jason Moore on 5/29/17.
+//  Copyright © 2017 Jason Moore. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view.")
+        updateCelsiusLabel()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        print("hours = \(hour)")
+        switch hour {
+        case 8 ... 20:
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            print("Its between 8am and 8pm")
+        default:
+            view.backgroundColor = UIColor.lightGray
+            print("Its between 9pm and 7am")
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBOutlet var celciusLabel: UILabel!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }else {
+            return nil
+        }
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celciusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        }else {
+            celciusLabel.text = "?"
+        }
+    }
+    
+    //Delegate that restricts white space, multiple decimals and all letters from textfield input
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let replacementTextHasLetters = string.rangeOfCharacter(from: NSCharacterSet.letters)
+        let replacementTextHasWhiteSpace = string.range(of: " ")
+        
+        if (existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil) ||
+            replacementTextHasLetters != nil || replacementTextHasWhiteSpace != nil {
+            return false
+        }else {
+            return true
+        }
+    }
+    
+}
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jason Moore on 6/3/17.
+//  Copyright © 2017 Jason Moore. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
